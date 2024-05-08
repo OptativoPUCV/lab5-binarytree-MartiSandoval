1b894bddd7b05fbe8fe2ac32ac6480f6b991d860
Your Name <you@example.com>
2024-05-08 05:39:11 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -23,7 +23,7 @@ int is_equal(TreeMap* tree, void* key1, void* key2){
     if(tree->lower_than(key1,key2)==0 &&  
         tree->lower_than(key2,key1)==0) return 1;
     else return 0;
-  return -1;
+  
 }
 
 
@@ -92,7 +92,7 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
       tree->current = aux;
       return aux->pair;
     }
-    else if (is_equal(tree, aux->pair->key, key) < 0) {
+    else if (is_equal(tree, aux->pair->key, key) > 0) {
       aux = aux->left;
     }
     else {
